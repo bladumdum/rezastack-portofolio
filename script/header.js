bb64400c77bea37ae10898d6aebebb2444afa4c6
Dwi Reza <dwireza1002@gmail.com>
2026-07-12 19:17:04 +0800

    feat: enhance navigation structure and styles; add smooth scrolling and mobile responsiveness

---
+++ script/header.js
@@ -0,0 +1,11 @@
+const menuList = document.querySelectorAll(".menu-list .menu-item");
+
+menuList.forEach((list) => {
+  list.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    menuList.forEach((item) => item.classList.remove("active"));
+
+    this.classList.add("active");
+  });
+});
