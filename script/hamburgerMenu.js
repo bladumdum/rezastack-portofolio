07a1394359345ae224061fde5c93fd7f2d39c6ef
Dwi Reza <dwireza1002@gmail.com>
2026-07-15 07:59:39 +0800

    fix: update navigation menu to highlight active link and improve styling

--- script/hamburgerMenu.js
+++ script/hamburgerMenu.js
@@ -1,5 +1,14 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".nav-menu");
+const menuList = document.querySelectorAll(".menu-list .menu-item a");
+
+menuList.forEach((link) => {
+  link.addEventListener("click", function () {
+    menuList.forEach((item) => item.classList.remove("active"));
+
+    this.classList.add("active");
+  });
+});
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
--- script/header.js
+++
@@ -1,9 +0,0 @@
-let menuList = document.querySelectorAll(".menu-list .menu-item");
-
-menuList.forEach((list) => {
-  list.querySelector("a").addEventListener("click", function (e) {
-    menuList.forEach((item) => item.classList.remove("active"));
-
-    this.classList.add("active");
-  });
-});
